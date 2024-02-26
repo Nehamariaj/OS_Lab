d2ed8ccd5d7aa70571f5704cac2d790663e63ce9
Neha Maria Joji <145547103+Nehamariaj@users.noreply.github.com>
2024-02-26 12:11:20 +0530

    Add files via upload

---
+++ NaturalNumbers.sh
@@ -0,0 +1,10 @@
+#print first 10 natural numbers
+#factorial of numbers
+#find the fibinoci series of 1st n
+#implement a simple calculator using switch
+echo "1st 10 natural numbers:"
+for((i=1;i<=10;i++))
+do
+echo -e "$i"
+done
+
