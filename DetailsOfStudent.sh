7b905cc05fe17b827723b8b8b98b63570568092f
Neha Maria Joji <145547103+Nehamariaj@users.noreply.github.com>
2024-02-26 12:23:41 +0530

    Add files via upload

---
+++ DetailsOfStudent.sh
@@ -0,0 +1,7 @@
+echo "Enter your Name,Semester,Batch,Roll No,Register Number"
+read name
+read sem
+read batch
+read rno
+read regno
+echo -e "DETAILS: \nName is $name \nSemester is $sem \nBatch is $batch \nRoll number is $rno \nRegister Number is $regno"
