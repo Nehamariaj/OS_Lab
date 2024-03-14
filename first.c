da3ba5ce919ebece09a4c1d22e942c5899fc75a3
Neha Maria Joji <145547103+Nehamariaj@users.noreply.github.com>
2024-03-14 07:24:51 +0530

    first.c

--- first.c
+++ first.c
@@ -3,7 +3,7 @@
 #include<stdlib.h>
 void main()
 {
-char *args[]={"./ex",NULL}; //"./ex" is used to store the compiled file instead of ./a.out
+char *args[]={"./neha",NULL}; //"./ex" is used to store the compiled file instead of ./a.out
 execvp(args[0],args);  //prints the content in new.c file
 printf("End");
 }
